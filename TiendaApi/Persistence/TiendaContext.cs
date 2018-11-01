ad5bce36890836f90a2cfd012a1e157021f04595
Yefry Bautista <yefryeldeadly@gmail.com>
2018-11-01 08:29:57 -0400

    Agregando Elementos Basicos del Api

---
+++ TiendaApi/Persistence/TiendaContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using System;
+
+namespace Persistence
+{
+    public class TiendaContext : DbContext
+    {
+        public TiendaContext(DbContextOptions<TiendaContext> options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
---
+++ TiendaApi/Services/ProductoServices.cs
@@ -0,0 +1,97 @@
+﻿using Model;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Services
+{
+    public interface IProductoServices
+    {
+        Producto Get(int id);
+        IEnumerable<Producto> GetAll();
+        bool Add(Producto producto);
+        bool Update(Producto producto);
+        bool Delete(int id);
+    }
+    
+    public class ProductoServices: IProductoServices
+    {
+        private readonly TiendaContext db;
+
+        public ProductoServices(TiendaContext _tiendaContext)
+        {
+            db = _tiendaContext;
+        }
+        public Producto Get(int id)
+        {
+            var producto = new Producto();
+            try
+            {
+                producto = db.Productos.Find(id);
+               
+            }
+            catch (Exception)
+            {
+
+            }
+            return producto;
+        }
+        public IEnumerable<Producto> GetAll()
+        {
+            var productos = new List<Producto>();
+            try
+            {
+                productos = db.Productos.ToList();
+            }
+            catch (Exception)
+            {
+
+            }
+            return productos;
+        }
+
+        public bool Add(Producto producto)
+        {
+            try
+            {
+                db.Productos.Add(producto);
+                db.SaveChanges();
+            }
+            catch (System.Exception)
+            {
+
+                return false;
+            }
+            return true;
+        }
+        public bool Update(Producto producto)
+        {
+            try
+            {
+                db.Productos.Update(producto);
+                db.SaveChanges();
+            }
+            catch (System.Exception)
+            {
+
+                return false;
+            }
+            return true;
+        }
+        public bool Delete(int id)
+        {
+            try
+            {
+                var producto = db.Productos.Find(id);
+                db.Productos.Remove(producto);
+                db.SaveChanges();
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
